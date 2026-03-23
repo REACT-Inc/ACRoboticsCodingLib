2a9f16c0f2dc2012c2f923af29509507b4b679b6
KoeRt888 <65965320+KoeRt888@users.noreply.github.com>
2026-03-23 11:06:45 -0400

    Updated the Simple Position Code
    Now includes the templates and more ideas for how some things will work within the class

--- src/main/java/com/acrobotics/v1/Simplify/SimplePositioningSystem.java
+++ src/main/java/com/acrobotics/v1/Simplify/SimplePositioningSystem.java
@@ -15,15 +15,43 @@ import com.pedropathing.paths.PathChain;
  */
 public class SimplePositioningSystem {
 
+    private Follower follower;
+
+    /**
+     * The Constructer for this
+     * @param follower Takes the follower from the pedropathing
+     */
+    public SimplePositioningSystem(Follower follower){
+        this.follower = follower;
+    }
+
+
+    /**
+     * Automatically goes to a designated position advoiding known obstcalls that are scanned from sensors around the robot
+     *
+     *
+     * @TODO  Make it so we can make everything on th feidl using a number of sensors around the robot to figure where stuff is
+     * @param endGoal the ending for the placement for the robot
+     */
+    public void navigate(Pose endGoal){
+        //Todo
+    }
+
+    /**
+     * Goes to a position on the feild automatically in the most optimal path
+     * @param endGoal the ending placement for the robot
+     */
+    public void goToPosition(Pose endGoal){
+        //TODO
+    }
 
     /**
      * This makes a path for you and does all of that stuff for you Cleaning up your code
-     * @param follower The pedropathing follower
      * @param startPose Starting pose
      * @param endPose End pose
      * @return the pathChain
      */
-    public PathChain constPathChain(Follower follower, Pose startPose, Pose endPose){
+    public PathChain constPathChain(Pose startPose, Pose endPose){
         PathChain pathMade = follower.pathBuilder()
                 .addPath(new BezierLine(startPose, endPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(), endPose.getHeading())
