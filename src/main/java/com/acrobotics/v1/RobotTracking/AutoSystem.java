044d828b6dbabffec2556009e37daf3a3e413b39
KoeRt888 <65965320+KoeRt888@users.noreply.github.com>
2026-03-23 11:14:21 -0400

    New Autosystem
    Enviormental Sensing

---
+++ src/main/java/com/acrobotics/v1/RobotTracking/AutoSystem.java
@@ -0,0 +1,8 @@
+package com.acrobotics.v1.RobotTracking;
+
+/**
+ * Handles the automatic actions based off Enviormental Senses, Such as the teleop Zoning thing how it auto shoots when in range,
+ * TODO
+ */
+public class AutoSystem {
+}
---
+++ src/main/java/com/acrobotics/v1/RobotTracking/EnviromentSensing.java
@@ -0,0 +1,8 @@
+package com.acrobotics.v1.RobotTracking;
+
+/**
+ * This would use the sensors around the robot that can see obstcalls and this class map and handles the knowlegde of where they are on
+ * the feild
+ */
+public class EnviromentSensing {
+}
