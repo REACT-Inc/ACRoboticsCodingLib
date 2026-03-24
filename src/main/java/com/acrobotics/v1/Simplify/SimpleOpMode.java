87c71f09fccae414253f5defaebf33ec7c331f93
KoeRt888 <65965320+KoeRt888@users.noreply.github.com>
2026-03-24 13:17:15 -0400

    Made custom opMode for simple Classes
    made custom opMode and added Telemetry Methods

---
+++ src/main/java/com/acrobotics/v1/Simplify/SimpleOpMode.java
@@ -0,0 +1,47 @@
+package com.acrobotics.v1.Simplify;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * A Simple Op mode extension to the main linear op mode so we can more direclty affect the programs
+ *
+ *
+ * You extend this class  when making your op mode!
+ *
+ *
+ * @author Cayden Riddle
+ * @version DEV.1
+ *
+ */
+public class SimpleOpMode extends OpMode {
+
+    /**
+     * @// TODO: 3/24/2026 Make this work fully in the init It needs to init our hardware system etc
+     * Really it shouldnt allow the user to init intill we have completed preChecks
+     */
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void init_loop(){
+
+    }
+}
--- src/main/java/com/acrobotics/v1/Simplify/SimpleTelemetry.java
+++ src/main/java/com/acrobotics/v1/Simplify/SimpleTelemetry.java
@@ -17,8 +17,8 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class SimpleTelemetry {
 
 
-    private final Telemetry ftcTelemetry;
-    private final PanelsTelemetry panelsTelemetry;
+    private static Telemetry ftcTelemetry;
+    private static PanelsTelemetry panelsTelemetry;
 
     /**
      * Consts the telemetry instance
@@ -29,6 +29,41 @@ public class SimpleTelemetry {
     public SimpleTelemetry(Telemetry ftc, PanelsTelemetry panels){
         ftcTelemetry = ftc;
         panelsTelemetry = panels;
+    }
+
+    /**
+     * Sends a message to the Displays so you can see it!
+     * @param title the Title of message
+     * @param message the message content
+     * @return Instance
+     */
+    public SimpleTelemetry sendMessage(Object title, Object message){
+        ftcTelemetry.addData((String) title, message);
+        panelsTelemetry.getTelemetry().addData((String) title, message);
+        return this;
+    }
+
+    /**
+     * Talks a message out loud VERY LOUD WARNING
+     *
+     * @implNote  THIS IS VERY LOUD
+     * @param message the message to speak
+     * @return Simpletelemetry Instance
+     */
+    public SimpleTelemetry talk(Object message){
+        ftcTelemetry.speak((String) message);
+        panelsTelemetry.getTelemetry().addData("Spoken Message", message);
+        return this;
+    }
+
+    /**
+     * Updates and pushes data to the Outputs
+     * 
+     * @// TODO: 3/24/2026 Call this in a centerual Looop Method system we make 
+     */
+    public static void update(){
+        ftcTelemetry.update();
+        panelsTelemetry.getTelemetry().update();
     }
     
 }
